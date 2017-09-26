724c4dae7c99372acfaded1e9ca9d2710a6f5e1c
MinWoo <dylan.byeon@daumkakao.com>
2017-09-26 23:12:16 +0900

    update prediction

--- predict/predict.py
+++ predict/predict.py
@@ -8,7 +8,7 @@ import coloredlogs
 from multiprocessing import Process
 coloredlogs.install(level=logging.INFO, milliseconds=True)
 
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 import mxnet as mx
 import numpy as np
@@ -43,17 +43,19 @@ def read_images(bson_path, csv_path):
 
     data = bson.decode_file_iter(open(bson_path, 'rb'))
 
-    idx = 0
+    product_count, image_count = 0, 0
     for c, d in enumerate(data):
         product_id = d.get('_id')
         category_id = d.get('category_id', None)  # This won't be in Test data
+        items = []
         for e, pic in enumerate(d['imgs']):
-            if e > 0:
-                continue  # use first image only
             picture = pic['picture']
-            item = (idx, picture, cate_dict[category_id] if category_id else product_id)
-            idx += 1
-            yield item  # id, picture, label, [label,]
+            item = (image_count, picture, cate_dict[category_id] if category_id else product_id)
+            items.append(item)
+            image_count += 1
+        product_count += 1
+        yield items  # list of [id, picture, label, [label,]]
+    logging.info('read finished (product:{}, image:{})'.format(product_count, image_count))
 
 
 class Tester(object):
@@ -91,13 +93,34 @@ def _reader(args):
     zmq_socket.bind('tcp://0.0.0.0:{port}'.format(port=args.zmq_port))
     logging.info('reader started (port: {port})'.format(port=args.zmq_port))
 
-    for item in read_images(args.bson, args.csv):
-        zmq_socket.send_pyobj((item[1], item[2]))
+    product_count = 0
+    for items in read_images(args.bson, args.csv):
+        zmq_socket.send_pyobj(items)
+        product_count += 1
 
     for _ in range(args.num_procs):
-        zmq_socket.send_pyobj((None, None))
+        zmq_socket.send_pyobj(None)
 
-    logging.info('reader finished')
+    logging.info('reader finished (product: {})'.format(product_count))
+
+
+def _do_forward(tester, batch_data, batch_ids):
+    output = tester.get_output(batch_data)
+    probs_dict = dict()
+    probs = output[0].asnumpy()
+    for i, _id in enumerate(batch_ids):
+        if _id is not None:
+            if _id in probs_dict:
+                probs_dict[_id] += probs[i]
+            else:
+                probs_dict[_id] = probs[i]
+    return probs_dict
+
+
+def _write_output(args, probs_dict, cid2cate):
+    for _id, prob in probs_dict.items():
+        args.output.write('{0:d},{1:d}\n'.format(_id, cid2cate[int(np.argmax(prob))]))
+        args.output.flush()
 
 
 def _predict(args):
@@ -121,36 +144,52 @@ def _predict(args):
     batch_ids = []
     term_count = 0
     product_count = 0
-    while True:
-        img, class_id = zmq_socket.recv_pyobj()
-        if img is None or class_id is None:
+
+    finished = False
+    while not finished:
+        images = zmq_socket.recv_pyobj()
+        if images is None:
             term_count += 1
             if term_count == args.num_procs:
-                break
-        else:
-            batch_data[len(batch_ids)] = img
-            batch_ids.append(class_id)
+                finished = True
+            continue
+
+        pad_forward = False
+        if len(images) + len(batch_ids) <= args.batch_size:
+            for img, class_id in images:
+                batch_data[len(batch_ids)] = img
+                batch_ids.append(class_id)
             product_count += 1
+        else:
+            pad_forward = True
 
-            if len(batch_ids) == args.batch_size:
-                __t1 = time.time()
-                output = tester.get_output(batch_data)
-                pred = mx.nd.argmax(output[0], axis=1).asnumpy()
-                __t2 = time.time()
-                for i in range(len(batch_ids)):
-                    args.output.write('{0:d},{1:d}\n'.format(batch_ids[i], cid2cate[int(pred[i])]))
-                    args.output.flush()
-                __t3 = time.time()
-                logging.info('[{4:8d}] batch:{0:.3f}, forward:{1:.3f}, write:{2:.3f} ({3:.3f})'.format(
-                    __t1-__t0, __t2-__t1, __t3-__t2, args.batch_size / (__t3-__t0), product_count))
-                __t0 = time.time()
-                batch_ids = []
+        if pad_forward or len(batch_ids) == args.batch_size:
+            __t1 = time.time()
+            probs_dict = _do_forward(tester, batch_data, batch_ids)
+            __t2 = time.time()
+            _write_output(args, probs_dict, cid2cate)
+            __t3 = time.time()
+            logging.info('[{4:8d}] batch:{0:.3f}, forward:{1:.3f}, write:{2:.3f} ({3:.3f}/s)'.format(
+                __t1-__t0, __t2-__t1, __t3-__t2, args.batch_size / (__t3-__t0), product_count))
+            __t0 = time.time()
 
-    output = tester.get_output(batch_data)
-    pred = mx.nd.argmax(output[0], axis=1).asnumpy()
-    for i in range(len(batch_ids)):
-        args.output.write('{0:d},{1:d}\n'.format(batch_ids[i], cid2cate[int(pred[i])]))
-        args.output.flush()
+            batch_ids[:] = []
+
+            if pad_forward and images:
+                for img, class_id in images:
+                    batch_data[len(batch_ids)] = img
+                    batch_ids.append(class_id)
+                product_count += 1
+
+    __t1 = time.time()
+    probs_dict = _do_forward(tester, batch_data, batch_ids)
+    __t2 = time.time()
+    _write_output(args, probs_dict, cid2cate)
+    __t3 = time.time()
+    logging.info('[{4:8d}] batch:{0:.3f}, forward:{1:.3f}, write:{2:.3f} ({3:.3f}/s)'.format(
+        __t1 - __t0, __t2 - __t1, __t3 - __t2, args.batch_size / (__t3 - __t0), product_count))
+    __t0 = time.time()
+
     logging.info('tester finished (product_count:{})'.format(product_count))
 
 
@@ -166,16 +205,19 @@ def _processor(args):
     ext_socket.connect('tcp://0.0.0.0:{port}'.format(port=args.zmq_port+1))
 
     while True:
-        img_bytes, class_id = zmq_socket.recv_pyobj()
-        if img_bytes is None or class_id is None:
+        items = zmq_socket.recv_pyobj()
+        if items is None:
             logging.info('processor finished')
-            ext_socket.send_pyobj((None, None))
+            ext_socket.send_pyobj(None)
             return
-        img = cv2.imdecode(np.fromstring(img_bytes, np.uint8), cv2.IMREAD_COLOR)
-        img = np.swapaxes(img, 0, 2)
-        img = np.swapaxes(img, 1, 2)
-        # img = np.expand_dims(img, axis=0)
-        ext_socket.send_pyobj((img, class_id))
+        images = []
+        for _, img_bytes, class_id in items:
+            img = cv2.imdecode(np.fromstring(img_bytes, np.uint8), cv2.IMREAD_COLOR)
+            img = np.swapaxes(img, 0, 2)
+            img = np.swapaxes(img, 1, 2)
+            # img = np.expand_dims(img, axis=0)
+            images.append((img, class_id))
+        ext_socket.send_pyobj(images)
 
 
 def main(args):
