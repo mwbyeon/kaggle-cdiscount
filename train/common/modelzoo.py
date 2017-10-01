6476cb438f8e88b1e239516043edcf5b92462002
MinWoo <dylan.byeon@daumkakao.com>
2017-10-02 02:29:21 +0900

    update

--- train/common/modelzoo.py
+++ train/common/modelzoo.py
@@ -4,30 +4,31 @@ from .util import download_file
 
 _base_model_url = 'http://data.mxnet.io/models/'
 _default_model_info = {
-    'imagenet1k-inception-bn': {'symbol':_base_model_url+'imagenet/inception-bn/Inception-BN-symbol.json',
-                             'params':_base_model_url+'imagenet/inception-bn/Inception-BN-0126.params'},
-    'imagenet1k-resnet-18': {'symbol':_base_model_url+'imagenet/resnet/18-layers/resnet-18-symbol.json',
-                             'params':_base_model_url+'imagenet/resnet/18-layers/resnet-18-0000.params'},
-    'imagenet1k-resnet-34': {'symbol':_base_model_url+'imagenet/resnet/34-layers/resnet-34-symbol.json',
-                             'params':_base_model_url+'imagenet/resnet/34-layers/resnet-34-0000.params'},
-    'imagenet1k-resnet-50': {'symbol':_base_model_url+'imagenet/resnet/50-layers/resnet-50-symbol.json',
-                             'params':_base_model_url+'imagenet/resnet/50-layers/resnet-50-0000.params'},
-    'imagenet1k-resnet-101': {'symbol':_base_model_url+'imagenet/resnet/101-layers/resnet-101-symbol.json',
-                             'params':_base_model_url+'imagenet/resnet/101-layers/resnet-101-0000.params'},
-    'imagenet1k-resnet-152': {'symbol':_base_model_url+'imagenet/resnet/152-layers/resnet-152-symbol.json',
-                             'params':_base_model_url+'imagenet/resnet/152-layers/resnet-152-0000.params'},
-    'imagenet1k-resnext-50': {'symbol':_base_model_url+'imagenet/resnext/50-layers/resnext-50-symbol.json',
-                             'params':_base_model_url+'imagenet/resnext/50-layers/resnext-50-0000.params'},
-    'imagenet1k-resnext-101': {'symbol':_base_model_url+'imagenet/resnext/101-layers/resnext-101-symbol.json',
-                             'params':_base_model_url+'imagenet/resnext/101-layers/resnext-101-0000.params'},
-    'imagenet1k-resnext-101-64x4d': {'symbol':_base_model_url+'imagenet/resnext/101-layers/resnext-101-64x4d-symbol.json',
-                                     'params':_base_model_url+'imagenet/resnext/101-layers/resnext-101-64x4d-0000.params'},
-    'imagenet11k-resnet-152': {'symbol':_base_model_url+'imagenet-11k/resnet-152/resnet-152-symbol.json',
-                             'params':_base_model_url+'imagenet-11k/resnet-152/resnet-152-0000.params'},
-    'imagenet11k-place365ch-resnet-152': {'symbol':_base_model_url+'imagenet-11k-place365-ch/resnet-152-symbol.json',
-                                          'params':_base_model_url+'imagenet-11k-place365-ch/resnet-152-0000.params'},
-    'imagenet11k-place365ch-resnet-50': {'symbol':_base_model_url+'imagenet-11k-place365-ch/resnet-50-symbol.json',
-                                         'params':_base_model_url+'imagenet-11k-place365-ch/resnet-50-0000.params'},
+    'imagenet1k-inception-bn': {'symbol': _base_model_url + 'imagenet/inception-bn/Inception-BN-symbol.json',
+                                'params': _base_model_url + 'imagenet/inception-bn/Inception-BN-0126.params'},
+    'imagenet1k-resnet-18': {'symbol': _base_model_url + 'imagenet/resnet/18-layers/resnet-18-symbol.json',
+                             'params': _base_model_url + 'imagenet/resnet/18-layers/resnet-18-0000.params'},
+    'imagenet1k-resnet-34': {'symbol': _base_model_url + 'imagenet/resnet/34-layers/resnet-34-symbol.json',
+                             'params': _base_model_url + 'imagenet/resnet/34-layers/resnet-34-0000.params'},
+    'imagenet1k-resnet-50': {'symbol': _base_model_url + 'imagenet/resnet/50-layers/resnet-50-symbol.json',
+                             'params': _base_model_url + 'imagenet/resnet/50-layers/resnet-50-0000.params'},
+    'imagenet1k-resnet-101': {'symbol': _base_model_url + 'imagenet/resnet/101-layers/resnet-101-symbol.json',
+                              'params': _base_model_url + 'imagenet/resnet/101-layers/resnet-101-0000.params'},
+    'imagenet1k-resnet-152': {'symbol': _base_model_url + 'imagenet/resnet/152-layers/resnet-152-symbol.json',
+                              'params': _base_model_url + 'imagenet/resnet/152-layers/resnet-152-0000.params'},
+    'imagenet1k-resnext-50': {'symbol': _base_model_url + 'imagenet/resnext/50-layers/resnext-50-symbol.json',
+                              'params': _base_model_url + 'imagenet/resnext/50-layers/resnext-50-0000.params'},
+    'imagenet1k-resnext-101': {'symbol': _base_model_url + 'imagenet/resnext/101-layers/resnext-101-symbol.json',
+                               'params': _base_model_url + 'imagenet/resnext/101-layers/resnext-101-0000.params'},
+    'imagenet1k-resnext-101-64x4d': {
+        'symbol': _base_model_url + 'imagenet/resnext/101-layers/resnext-101-64x4d-symbol.json',
+        'params': _base_model_url + 'imagenet/resnext/101-layers/resnext-101-64x4d-0000.params'},
+    'imagenet11k-resnet-152': {'symbol': _base_model_url + 'imagenet-11k/resnet-152/resnet-152-symbol.json',
+                               'params': _base_model_url + 'imagenet-11k/resnet-152/resnet-152-0000.params'},
+    'imagenet11k-place365ch-resnet-152': {'symbol': _base_model_url + 'imagenet-11k-place365-ch/resnet-152-symbol.json',
+                                          'params': _base_model_url + 'imagenet-11k-place365-ch/resnet-152-0000.params'},
+    'imagenet11k-place365ch-resnet-50': {'symbol': _base_model_url + 'imagenet-11k-place365-ch/resnet-50-symbol.json',
+                                         'params': _base_model_url + 'imagenet-11k-place365-ch/resnet-50-0000.params'},
 }
 
 
@@ -45,10 +46,10 @@ def download_model(model_name, dst_dir='./', meta_info=None):
     symbol_path = model_name + '-symbol.json'
     params_path = model_name + '-0000.params'
     if not os.path.exists(symbol_path):
-        print( symbol_path )
+        print(symbol_path)
         download_file(meta['symbol'], symbol_path)
     assert 'params' in meta, "mssing parameter file url"
     if not os.path.exists(params_path):
-        print( params_path )
+        print(params_path)
         download_file(meta['params'], params_path)
     return (model_name, 0)
--- train/run_finetune.sh
+++ train/run_finetune.sh
@@ -4,24 +4,26 @@ ROOT=/home/deploy/dylan/projects/kaggle-cdiscount
 
 C2048_NUM_EXAMPLES=11095745
 C5270_NUM_EXAMPLES=11752908
+UNDER10K_NUM_EXAMPLES=6859873
 MODEL=resnext-101
 
 MXNET_CUDNN_AUTOTUNE_DEFAULT=0 python3 fine-tune.py \
     --gpus              0,1,2,3,4,5,6,7 \
     --kv-store          device \
-    --pretrained-model  ${ROOT}/train/model/imagenet1k-${MODEL} \
+    --pretrained-model  ${ROOT}/train/checkpoints/${MODEL}/imagenet1k-${MODEL} \
     --model-prefix      ${ROOT}/train/checkpoints/${MODEL}/imagenet1k-${MODEL} \
+    --fix-last-layer \
     --data-train        ${ROOT}/data/cdiscount_c5270_train.rec \
-    --data-val          ${ROOT}/data/cdiscount_c5270_val.rec  \
+    --data-val          ${ROOT}/data/cdiscount_c5270_val.rec \
     --image-shape       3,180,180 \
     --data-nthread      6 \
     --lr                0.0005 \
     --lr-factor         0.2 \
     --lr-step-epochs    6,10,13 \
     --mom               0.9 \
-    --wd                0.00001 \
+    --wd                0.00004 \
     --num-epoch         15 \
-    --load-epoch        0 \
+    --load-epoch        4 \
     --top-k             5 \
     --batch-size        512 \
     --num-classes       5270 \
