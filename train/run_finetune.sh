bdfe2c42c7b4aa35153bbe487cbccc7e8d6082f3
MinWoo <dylan.byeon@daumkakao.com>
2017-10-08 02:57:43 +0900

    use refined dataset

--- train/run_finetune.sh
+++ train/run_finetune.sh
@@ -2,33 +2,32 @@
 
 ROOT=/home/deploy/dylan/projects/kaggle-cdiscount
 
-C2048_NUM_EXAMPLES=11095745
 C5270_NUM_EXAMPLES=11752908
-UNDER10K_NUM_EXAMPLES=6859873
-MODEL=dpn92-5k
+REFINE_NUM_EXAMPLES=7310313
+MODEL=resnext-101
 
 MXNET_CUDNN_AUTOTUNE_DEFAULT=0 python3 -u fine-tune.py \
     --gpus              0,1,2,3,4,5,6,7 \
     --kv-store          device \
-    --pretrained-model  ${ROOT}/train/model/DPNs/dpn92-5k \
-    --model-prefix      ${ROOT}/train/checkpoints/dpn92-5k/dpn92-5k \
-    --layer-before-fullc flatten \
-    --data-train        ${ROOT}/data/cdiscount_c5270_train.rec \
-    --data-val          ${ROOT}/data/cdiscount_c5270_val.rec \
+    --pretrained-model  ${ROOT}/train/checkpoints/${MODEL}/imagenet1k-${MODEL} \
+    --model-prefix      ${ROOT}/train/checkpoints/${MODEL}/imagenet1k-${MODEL} \
+    --fix-last-layer \
+    --data-train        ${ROOT}/data/refined_train.rec \
+    --data-val          ${ROOT}/data/refined_val.rec \
     --image-shape       3,180,180 \
     --data-nthread      6 \
     --optimizer         adam \
-    --lr                0.0001 \
+    --lr                0.0005 \
     --lr-factor         0.2 \
-    --lr-step-epochs    8,16,20 \
+    --lr-step-epochs    8,18,20 \
+    --num-epoch         20 \
+    --load-epoch        16 \
     --mom               0.9 \
     --wd                0.00004 \
-    --num-epoch         20 \
-    --load-epoch        0 \
     --top-k             5 \
     --batch-size        512 \
     --num-classes       5270 \
-    --num-examples      ${C5270_NUM_EXAMPLES} \
+    --num-examples      ${REFINE_NUM_EXAMPLES} \
     --rgb-mean          0,0,0 \
     --rgb-scale         1.0 \
     --random-crop       1 \
