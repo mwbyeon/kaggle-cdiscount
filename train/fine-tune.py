b20c89340cfd1ffdf31e30bbb0de14e7c27c2aad
Dylan.Byeon <dylan.byeon@kakaocorp.com>
2017-11-02 01:48:07 +0900

    add lsoftmax

--- train/fine-tune.py
+++ train/fine-tune.py
@@ -10,7 +10,7 @@ import mxnet as mx
 from nce import nce_loss
 
 
-def get_fine_tune_model(symbol, arg_params, num_classes, layer_name):
+def get_fine_tune_model(symbol, arg_params, num_classes, layer_name, use_lsoftmax=False):
     """
     symbol: the pre-trained network symbol
     arg_params: the argument parameters of the pre-trained model
@@ -18,9 +18,19 @@ def get_fine_tune_model(symbol, arg_params, num_classes, layer_name):
     layer_name: the layer name before the last fully-connected layer
     """
     all_layers = symbol.get_internals()
-    net = all_layers[layer_name + '_output']
-    net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc')
-    net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
+    net = all_layers[layer_name + '_output']  # embedding
+    label = mx.sym.Variable('softmax_label')
+    if use_lsoftmax:
+        beta_min, beta = 0.0, 100
+        scale = 0.99
+        margin = 2
+        net = mx.sym.LSoftmax(data=net, label=label, num_hidden=num_classes,
+                              beta=beta, margin=margin, scale=scale,
+                              beta_min=beta_min, verbose=True)
+    else:
+        net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc')
+    net = mx.symbol.SoftmaxOutput(data=net, label=label, name='softmax')
+
     if not args.fix_last_layer:
         new_args = dict({k: arg_params[k] for k in arg_params if 'fc' not in k})
     else:
