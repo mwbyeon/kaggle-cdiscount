a1e2b582e6cbbca635d3f769c17b14b5d30510a6
Dylan.Byeon <dylan.byeon@kakaocorp.com>
2017-12-06 00:20:00 +0900

    update prediction

--- predict/predict.py
+++ predict/predict.py
@@ -123,13 +123,13 @@ def read_images(bson_path, csv_path, cut=None):
             category_id = d.get('category_id', None)  # This won't be in Test data
             items = []
             prod_md5_set = set()
-            for e, pic in enumerate(d['imgs']):
+            for i, pic in enumerate(d['imgs']):
                 img_bytes = pic['picture']
                 h = hashlib.md5(img_bytes).hexdigest()
                 if args.product_unique_md5 and h in prod_md5_set:
                     continue
                 prod_md5_set.add(h)
-                item = (image_count, img_bytes, product_id)
+                item = (product_id, i, img_bytes)
                 items.append(item)
                 image_count += 1
             product_count += 1
@@ -155,57 +155,68 @@ def _func_reader(args):
     logging.info('reader finished (product: {})'.format(product_count))
 
 
-def _do_forward(testers, batch_data, batch_ids, batch_raw, cate3_dict, md5_dict=None, md5_type=None, cate_level=3):
-    probs_dict = dict()
-    for tester in testers:
-        output = tester.get_output(batch_data)
+def _do_forward(models, batch_data, batch_ids, batch_raw, cate3_dict, md5_dict=None, md5_type=None, cate_level=3):
+    probs_dict = defaultdict(lambda: defaultdict(list))
+    for model_id, model in enumerate(models):
+        output = model.get_output(batch_data)
         probs = output[0].asnumpy()
-        for i, _id in enumerate(batch_ids):
-            if _id is not None:
-                p = probs[i]  # softmax
+        for i, (product_id, image_id) in enumerate(batch_ids):
+            if product_id is not None:
+                prob = probs[i]  # softmax
                 if md5_dict:
                     h = hashlib.md5(batch_raw[i]).hexdigest()
                     if h in md5_dict:
                         if md5_type == 'unique' and len(md5_dict[h]) == 1:  # BEST!
-                            p = np.zeros(probs.shape[1:])
+                            prob = np.zeros(probs.shape[1:])
                             most_label, most_count = md5_dict[h].most_common(1)[0]
                             class_id = cate3_dict[most_label]['cate1_sub_class_id'] if cate_level == 1 else cate3_dict[most_label]['cate3_class_id']
-                            p[class_id] = 10.0
+                            prob[class_id] = 10.0
                         elif md5_type == 'majority':
-                            p = np.zeros(probs.shape[1:])
+                            prob = np.zeros(probs.shape[1:])
                             most_label, most_count = md5_dict[h].most_common(1)[0]
                             class_id = cate3_dict[most_label]['cate1_sub_class_id'] if cate_level == 1 else cate3_dict[most_label]['cate3_class_id']
-                            p[class_id] = 1.0
+                            prob[class_id] = 1.0
                         elif md5_type == 'l1':
-                            p = np.zeros(probs.shape[1:])
+                            prob = np.zeros(probs.shape[1:])
                             for cate, cnt in md5_dict[h].items():
                                 class_id = cate3_dict[cate]['cate1_sub_class_id'] if cate_level == 1 else cate3_dict[cate]['cate3_class_id']
-                                p[class_id] = cnt
-                            p /= sum(list(md5_dict[h].values()))
+                                prob[class_id] = cnt
+                            prob /= sum(list(md5_dict[h].values()))
                         elif md5_type == 'l2':
-                            p = np.zeros(probs.shape[1:])
+                            prob = np.zeros(probs.shape[1:])
                             for cate, cnt in md5_dict[h].items():
                                 class_id = cate3_dict[cate]['cate1_sub_class_id'] if cate_level == 1 else cate3_dict[cate]['cate3_class_id']
-                                p[class_id] = cnt
-                            p /= np.linalg.norm(list(md5_dict[h].values()))
+                                prob[class_id] = cnt
+                            prob /= np.linalg.norm(list(md5_dict[h].values()))
                         elif md5_type == 'softmax':
-                            p = np.zeros(probs.shape[1:])
+                            prob = np.zeros(probs.shape[1:])
                             for cate, cnt in md5_dict[h].items():
                                 class_id = cate3_dict[cate]['cate1_sub_class_id'] if cate_level == 1 else cate3_dict[cate]['cate3_class_id']
-                                p[class_id] = cnt
-                            e_p = np.exp(p - np.max(p))
-                            p = e_p / e_p.sum()
-
-                if _id in probs_dict:
-                    if args.multi_view_mean == 'arithmetic':
-                        probs_dict[_id] += p
-                    elif args.multi_view_mean == 'geometric':
-                        probs_dict[_id] *= p
-                    elif args.multi_view_mean == 'max':
-                        probs_dict[_id] = np.maximum(probs_dict[_id], p)
-                else:
-                    probs_dict[_id] = p
+                                prob[class_id] = cnt
+                            e_p = np.exp(prob - np.max(prob))
+                            prob = e_p / e_p.sum()
+                probs_dict[product_id][image_id].append((model_id, prob))
     return probs_dict
+
+
+def _predict(probs_dict):
+    result = dict()
+    for product_id, prod in probs_dict.items():
+        product_prob = None
+        for image_id, image in prod.items():
+            image_prob = None
+            for model_id, prob in image:
+                if image_prob is None:
+                    image_prob = prob
+                else:
+                    image_prob += prob
+            image_prob /= len(image_prob)
+            if product_prob is None:
+                product_prob = image_prob
+            else:
+                product_prob *= image_prob
+        result[product_id] = int(np.argmax(product_prob))
+    return result
 
 
 def _func_predict(args):
@@ -270,10 +281,10 @@ def _func_predict(args):
 
         pad_forward = False
         if len(images) + len(batch_ids) <= args.batch_size:
-            for img, product_id, raw in images:
+            for img, product_id, image_id, image_raw in images:
                 batch_data[len(batch_ids)] = img
-                batch_ids.append(product_id)
-                batch_raw.append(raw)
+                batch_ids.append((product_id, image_id))
+                batch_raw.append(image_raw)
             product_count += 1
             bar.update(n=1)
         else:
@@ -282,11 +293,11 @@ def _func_predict(args):
         if pad_forward or len(batch_ids) == args.batch_size:
             __t1 = time.time()
             probs_dict = _do_forward(testers, batch_data, batch_ids, batch_raw, cate3_dict, md5_dict, args.md5_dict_type, args.cate_level)
+            preds_dict = _predict(probs_dict)
             __t2 = time.time()
-            for _id, prob in probs_dict.items():
-                pred = int(np.argmax(prob))
-                if _id in ground_truths:
-                    label = ground_truths.get(_id)
+            for product_id, pred in preds_dict.items():
+                if product_id in ground_truths:
+                    label = ground_truths.get(product_id)
                     catetory_count_dict[label] += 1
                     if label == pred:  # correct
                         correct_count += 1
@@ -295,7 +306,7 @@ def _func_predict(args):
                         incorrect_count_dict[label][pred] += 1
                 if writer:
                     cate_id = cate3_dict[pred]['cate_id'] if args.cate_level == 3 else pred
-                    writer.write('{0:d},{1:d}\n'.format(_id, cate_id))
+                    writer.write('{0:d},{1:d}\n'.format(product_id, cate_id))
                     writer.flush()
             __t3 = time.time()
             bar.write('[{0:8d}] acc={1:.6f} batch:{2:.3f}, forward:{3:.3f}, write:{4:.3f} ({5:.1f}images/s)'.format(
@@ -306,21 +317,21 @@ def _func_predict(args):
             batch_raw[:] = []
 
             if pad_forward and images:
-                for img, class_id, raw in images:
+                for img, product_id, image_id, image_raw in images:
                     batch_data[len(batch_ids)] = img
-                    batch_ids.append(class_id)
-                    batch_raw.append(raw)
+                    batch_ids.append((product_id, image_id))
+                    batch_raw.append(image_raw)
                 product_count += 1
                 bar.update(n=1)
             __t0 = time.time()
 
     __t1 = time.time()
     probs_dict = _do_forward(testers, batch_data, batch_ids, batch_raw, cate3_dict, md5_dict, args.md5_dict_type, args.cate_level)
+    preds_dict = _predict(probs_dict)
     __t2 = time.time()
-    for _id, prob in probs_dict.items():
-        pred = int(np.argmax(prob))
-        if _id in ground_truths:
-            label = ground_truths[_id]
+    for product_id, pred in preds_dict.items():
+        if product_id in ground_truths:
+            label = ground_truths[product_id]
             catetory_count_dict[label] += 1
             if label == pred:  # correct
                 correct_count += 1
@@ -329,7 +340,7 @@ def _func_predict(args):
                 incorrect_count_dict[label][pred] += 1
         if writer:
             cate_id = cate3_dict[pred]['cate_id'] if args.cate_level == 3 else pred
-            writer.write('{0:d},{1:d}\n'.format(_id, cate_id))
+            writer.write('{0:d},{1:d}\n'.format(product_id, cate_id))
             writer.flush()
     __t3 = time.time()
     bar.write('[{0:8d}] acc={1:.6f} batch:{2:.3f}, forward:{3:.3f}, write:{4:.3f} ({5:.3f}/s)'.format(
@@ -382,23 +393,22 @@ def _func_processor(args):
             ext_socket.send_pyobj(None)
             return
         images = []
-        for _, img_bytes, product_id in items:
+        for product_id, image_id, img_bytes in items:
             img = cv2.imdecode(np.fromstring(img_bytes, np.uint8), cv2.IMREAD_COLOR)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             if args.resize > 0:
                 img = cv2.resize(img, (args.resize, args.resize), interpolation=cv2.INTER_CUBIC)
             if args.multi_view >= 0:  # 0.778900
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                images.append((_hwc_to_chw(img), product_id, img_bytes))
+                images.append((_hwc_to_chw(img), product_id, image_id, img_bytes))
             if args.multi_view >= 1:  # 0.780900 (+0.0020)
                 img_flip = cv2.flip(img, flipCode=1)
-                images.append((_hwc_to_chw(img_flip), product_id, img_bytes))
+                images.append((_hwc_to_chw(img_flip), product_id, image_id, img_bytes))
             if args.multi_view >= 2:  # 0.781000 (+0.0001)
                 img_crop = cv2.resize(img[5:-5, 5:-5, :], tuple(data_shape[1:]))
-                images.append((_hwc_to_chw(img_crop), product_id, img_bytes))
+                images.append((_hwc_to_chw(img_crop), product_id, image_id, img_bytes))
             if args.multi_view >= 3:  # 0.781700 (+0.0007)
                 img_flip = cv2.flip(img, flipCode=0)
-                images.append((_hwc_to_chw(img_flip), product_id, img_bytes))
-
+                images.append((_hwc_to_chw(img_flip), product_id, image_id, img_bytes))
         ext_socket.send_pyobj(images)
 
 
